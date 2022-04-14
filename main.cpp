89931ed455bae4d1f68ba8c0d1409f4c5ead0367
pqx <l.r.slegt@gmail.com>
2022-04-14 03:16:03 +0200

    final commit

--- main.cpp
+++ main.cpp
@@ -65,13 +65,13 @@ int main(){
 	std::cout << std::endl;
 	
 	std::cout << "(d > e) && (d > f) : " << ((d > e) && (d > f)) << std::endl; // true
-	std::cout << "(d==e) || (e <= f ) : " << ((d==e) || (e <= f ) ) << std::endl;
-	std::cout << "(d < e) || (d > f) : " << ((d < e) || (d > f)) << std::endl;
-	std::cout << "(f > e) || (d < f) : " << ((f > e) || (d < f)) << std::endl;
-	std::cout << "(d > f) && (f <= d) : " << ((d > f) && (f <= d)) << std::endl;
-	std::cout << "(d > e) && (d <= f) : " << ((d > e) && (d <= f)) << std::endl;
-	std::cout << "(! a) && (d == e) : " << ((! a) && (d == e)) << std::endl;
-	std::cout << "(! a) && (d == e) : " << ((! a) && (d == e)) << std::endl;
+	std::cout << "(d==e) || (e <= f ) : " << ((d==e) || (e <= f ) ) << std::endl; // false
+	std::cout << "(d < e) || (d > f) : " << ((d < e) || (d > f)) << std::endl; // true
+	std::cout << "(f > e) || (d < f) : " << ((f > e) || (d < f)) << std::endl; // false
+	std::cout << "(d > f) && (f <= d) : " << ((d > f) && (f <= d)) << std::endl; // true
+	std::cout << "(d > e) && (d <= f) : " << ((d > e) && (d <= f)) << std::endl; // false
+	std::cout << "(! a) && (d == e) : " << ((! a) && (d == e)) << std::endl; // false
+	std::cout << "(! a) && (d == e) : " << ((! a) && (d == e)) << std::endl; // false
    
     return 0;
 }
